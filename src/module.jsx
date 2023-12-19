4f143ee579cb0cc7e1e19fcca3938dfeb93ad5bc
Galina Kostina <Kostina13@ya.ru>
2023-12-19 18:41:57 +0300

    react-template

---
+++ src/module.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import avatar from './img/foto1.jpg';
+
+const Module = () => (
+    <>
+        <h1>Hello, react</h1>
+        <img src={avatar}/>
+    </>
+)
+export default Module;
